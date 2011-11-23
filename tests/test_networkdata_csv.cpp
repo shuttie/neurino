802989cf557411f49e99479f9f5b0d0376427fa1
Roman Grebennikov <grebennikov.roman@gmail.com>
2011-11-23 13:13:04 +0400

    fix failing csv test because of api change

--- tests/test_networkdata_csv.cpp
+++ tests/test_networkdata_csv.cpp
@@ -6,7 +6,7 @@
 int main(int argc, char** argv) {
     if (argc == 2) {
         string sdir(argv[1]);
-        NetworkDataCsv data(47,4,sdir+"/tests/input.csv");
+        NetworkDataCsv data(47,4,sdir+"/tests/input.csv"," ");
         if (data.samples.size() == 1000)
             printf("PASS\n");
         else
